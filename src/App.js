720a17e94f071454d94a3fde4087bb8db8d10527
Yasrah Kapadia <yasrah.kapadiaa@gmail.com>
2020-12-17 00:02:14 -0600

    updated the header image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css';
-import headerImage from "./images/headerimage.png";
+import headerImage from "./images/globe.png";
 function App() {
   return (
     <div className="App">
       <header>
-        <img src={headerImage} alt="Haapy World" /> <h1>What temperature is it ?</h1>
+        <img src={headerImage} alt="Hapy World" /> 
+<h1>What temperature is it ?</h1>
       </header>
       <div className="container">
     
