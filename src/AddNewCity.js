7a7ba2d984745299227231706c646cb4b30a5e86
Yasrah Kapadia <yasrah.kapadiaa@gmail.com>
2020-12-18 00:07:34 -0600

    Added NewCityCard.js and Weather.js file

---
+++ src/AddNewCity.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import './AddNewCity.css';
+
+export default function AddNewCity(){
+const[click, setClick]=useState(false)
+
+    function handleCityCard(event){
+        event.preventDefault();
+        setClick(true);
+
+    }
+
+    if (click){
+        return(
+            <div className="AddNewCity">
+<Weather defaultCity={props.defaultCity} />
+            </div>
+        );      
+    } else {
+
+    return(
+       <div className="AddNewCity">
+           <div className="CityCard">
+               <button type="button" title="Add a new place" className="btn btn-light mt-6" onClick={handleCityCard}>
+
+               </button>
+           </div>
+       </div> 
+    );
+}
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from "react";
+import Weather from "./Weather";
+import AddNewCity from "./AddNewCity";
 import './App.css';
 import headerImage from "./images/globe.png";
 function App() {
   return (
     <div className="App">
+        <div className="container">
       <header>
         <img src={headerImage} alt="Hapy World" /> 
 <h1>What temperature is it ?</h1>
       </header>
-      <div className="container">
     
+      <div className="citiesWeather row">
+        <div className="col">
+         <Weather />
+        </div>
+        <div className="col">
+         <AddNewCity defaulltCity="New York"/>
+        </div>
+        <div className="col">
+          3
+        </div>
+        <div className="col">
+          4
+        </div>
+        <div className="col">
+          5
+        </div>
+        <div className="col">
+          6
+        </div>
+      </div>
+
+      <footer className="mt-5">
+        <p>
+          💻 Open-source code by Yasrah Kapadia
+        </p>
+      </footer>
       </div>
     </div>
   );
